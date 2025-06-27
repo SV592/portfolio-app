32068e6271c0e8ca489d6ff34b966ced549234f2
Shaquille Pearson <shaquillepearson9@gmail.com>
2025-06-26 21:23:05 -0400

    about section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Funnel_Sans, Funnel_Display } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const geistSans = Funnel_Sans({
+  variable: "--font-funnel-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const geistMono = Funnel_Display({
+  variable: "--font-funnel-display",
   subsets: ["latin"],
 });
 
